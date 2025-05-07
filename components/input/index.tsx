9bc0e73ddb7f173383425a016c8bbfe637fb05ab
Gabriel Reis <61790149+grgm2001@users.noreply.github.com>
2025-05-06 21:32:32 -0300

    Delete components directory

--- components/input/index.tsx
+++
@@ -1,6 +0,0 @@
-import { TextInput, TextInputProps } from "react-native";
-import { styles } from "../input/style";
-
-export function Input({...rest}: TextInputProps){
-    return <TextInput style={styles.input} {...rest}/>
-}
